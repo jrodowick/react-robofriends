764811c7d54133ee7850ffb3bc68b79f9ff5bf2b
jrodowick <jrodowick@mail.csuchico.edu>
2019-03-11 14:22:22 -0700

    Adds background gradient/Font for header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import './App.css';
 import { robots } from './robots';
 
 class App extends Component {
